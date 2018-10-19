77ffe335dc3483e18aa63a4367cf2bcbf9e2fa40
unknown <wkogucki@gmail.com>
2018-10-19 20:26:39 +0200

    Creating a rounter to main page and search page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from './BookList.js'
@@ -6,25 +7,21 @@ import BookSearch from './BookSearch.js'
 
 class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <BookSearch />
-        ) : (
-          <BookList />
-        )}
+      <Route exact path='/' render = {() => (
+        <BookList />
+      )}/>
+      <Route path='booksearch' render = {() => {
+                <BookSearch />
+      }}/>
       </div>
     )
   }
 }
+
 export default BooksApp;
