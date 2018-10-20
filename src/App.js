01d2d4160343ddbb1c63f54d50859d575da54581
unknown <wkogucki@gmail.com>
2018-10-20 13:29:21 +0200

    Fetching the books from the server

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from './BookList.js'
 import BookSearch from './BookSearch.js'
 
 class BooksApp extends Component {
   state = {
-
+    books: []
   }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  }
+
 
   render() {
     return (
       <div className="app">
       <Route exact path='/' render = {() => (
-        <BookList />
+        <BookList
+        books={this.state.books}
+         />
       )}/>
       <Route path='/booksearch' render = {() => (
         <BookSearch />
